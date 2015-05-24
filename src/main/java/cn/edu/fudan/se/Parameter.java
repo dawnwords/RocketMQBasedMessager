e8b62385e0b3942492eb232161f4fe2758a38b58
Gu Jingxiao <gujingxiao@gmail.com>
2015-05-24 19:25:16 +0800

    + Parameter for Topic & Tag & Group settings

---
+++ src/main/java/cn/edu/fudan/se/Parameter.java
@@ -0,0 +1,18 @@
+package cn.edu.fudan.se;
+
+/**
+ * Created by Dawnwords on 2015/5/24.
+ */
+public class Parameter {
+    public static final String TOPIC = "test";
+    public static final String INVOKER_CONSUMER_GROUP = "invoker";
+    public static final String INVOKER_PRODUCER_GROUP = "invoker";
+    public static final String INVOKER_KEY = "request";
+
+    public static final String RESPONSOR_CONSUMER_GROUP = "responsor";
+    public static final String RESPONSOR_PRODUCER_GROUP = "responsor";
+    public static final String RESPONSOR_KEY = "response";
+
+    public static final String RESPONSE_TAG = "lecture_response";
+    public static final String REQUEST_TAG = "lecture_request";
+}
